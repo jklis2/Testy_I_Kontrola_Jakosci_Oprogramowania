0b20c089b1e1d01ed1cdd85ac77724b08f9a6673
Jakub Kliś <jakubklis2201@gmail.com>
2022-05-22 13:35:07 +0200

    Tutorial 2

--- SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
@@ -8,27 +8,33 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using SeleniumCSharpTutorials.BaseClass;
 using System.Threading;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumCSharpTutorials
 {
     [TestFixture]
     public class TestClass : BaseTest
     {
-        [Test]
+        [Test, Category("Smoke Testing")]
         public void TestMethod1()
         {
             IWebElement emailTextField=driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
+            IWebElement monthDropdownList=driver.FindElement(By.XPath(".//*[@id='month']"));
+            SelectElement element = new SelectElement(monthDropdownList);
+            element.SelectByIndex(1); //Select by index
+            element.SelectByText("Mar"); //Select by text
+            element.SelectByValue("6"); //Select by value
         }
 
-        [Test]
+        [Test, Category("Rehression Testing")]
         public void TestMethod2()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
         }
 
-        [Test]
+        [Test, Category("Smoke Testing")]
         public void TestMethod3()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
